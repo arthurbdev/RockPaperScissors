3fbd9188d8e81d35d89dd20ecb1dc863b5fd765d
Arthur Bais <artb@tutamail.com>
2023-06-10 17:53:14 +0300

    Add play round function that defines the winner of the round

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-console.log("Rock Paper Scissors")
+function capitalizeFirstLetter(str) { return str.charAt(0).toUpperCase() + str.slice(1) }
+
 choice = ['Rock', 'Paper', 'Scissors']
 
 function getComputerChoice() { return choice[~~(Math.random() * choice.length)] }
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
+
+  if(playerSelection == computerSelection) {
+  {
+      return "Draw! " +  capitalizeFirstLetter(playerSelection) + " and " + capitalizeFirstLetter(computerSelection)
+    }
+  }
+  else if((playerSelection == "scissors" && computerSelection == "paper") || 
+          (playerSelection == "paper" && computerSelection == "rock") ||
+          (playerSelection == "rock" && computerSelection == "scissors"))
+          { 
+    return "You win! " + capitalizeFirstLetter(playerSelection) + " beats " + capitalizeFirstLetter(computerSelection) 
+  }
+  else { 
+    return "You lose! " + capitalizeFirstLetter(computerSelection) + " beats " + capitalizeFirstLetter(playerSelection) 
+  }
+}
