c0354e1339728d7e830f252ec915c1d34ae0184c
Arthur Bais <artb@tutamail.com>
2023-06-10 18:08:57 +0300

    Fix formatting

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 function capitalizeFirstLetter(str) { return str.charAt(0).toUpperCase() + str.slice(1) }
 
-choice = ['Rock', 'Paper', 'Scissors']
-
 function getComputerChoice() { return choice[~~(Math.random() * choice.length)] }
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase()
   computerSelection = computerSelection.toLowerCase()
 
-  if(playerSelection == computerSelection) {
-  {
-      return "Draw! " +  capitalizeFirstLetter(playerSelection) + " and " + capitalizeFirstLetter(computerSelection)
-    }
+  if(playerSelection == computerSelection) { 
+    return "Draw! " +  capitalizeFirstLetter(playerSelection) + 
+           " and " + capitalizeFirstLetter(computerSelection)
   }
   else if((playerSelection == "scissors" && computerSelection == "paper") || 
           (playerSelection == "paper" && computerSelection == "rock") ||
           (playerSelection == "rock" && computerSelection == "scissors"))
           { 
     playerScore++
-    return "You win! " + capitalizeFirstLetter(playerSelection) + " beats " + capitalizeFirstLetter(computerSelection) 
+    return "You win! " + capitalizeFirstLetter(playerSelection) +
+           " beats " + capitalizeFirstLetter(computerSelection) 
   }
   else { 
     compScore++
-    return "You lose! " + capitalizeFirstLetter(computerSelection) + " beats " + capitalizeFirstLetter(playerSelection) 
+    return "You lose! " + capitalizeFirstLetter(computerSelection) + 
+           " beats " + capitalizeFirstLetter(playerSelection) 
   }
 }
 
@@ -29,8 +29,10 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   while (true) {
     let playerChoice=prompt("Rock, Paper or Scissors?")
+
     console.log(playRound(playerChoice,getComputerChoice()))
     console.log("Score:\nYou - " + playerScore + " Computer - " + compScore)
+
     if(playerScore == drawTo) {
       console.log("You won the game. Congrats..")
       return
@@ -42,6 +44,7 @@ function game() {
   }
 }
 
+choice = ['Rock', 'Paper', 'Scissors']
 let playerScore = compScore = 0
 let drawTo = 5
 game()
