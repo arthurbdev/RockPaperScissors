6b1e9d7ae138069bee602837af4c422124342e58
Arthur Bais <artb@tutamail.com>
2023-06-13 16:31:25 +0300

    Add UI in html, rewrite javascript logic for UI

--- main.js
+++ main.js
@@ -1,50 +1,75 @@
-function capitalizeFirstLetter(str) { return str.charAt(0).toUpperCase() + str.slice(1) }
+const rock = document.querySelector('#rock-btn')
+const paper = document.querySelector('#paper-btn')
+const scissors = document.querySelector('#scissors-btn')
+
+const results = document.querySelector('#results')
+const score = document.querySelector('#score')
+
+const playerScoreElement = document.querySelector('#player-score')
+const computerScoreElement = document.querySelector('#computer-score')
+
+const winnerElement = document.querySelector('#win')
+const playagainButton = document.querySelector('#play-again')
+playagainButton.addEventListener('click', playAgain)
+
+rock.addEventListener('click', () => playRound('rock'))
+paper.addEventListener('click', () => playRound('paper'))
+scissors.addEventListener('click', () => playRound('scissors'))
+
+let choice = ['rock', 'paper', 'scissors']
+let playerScore = compScore = 0
+const rounds = 3
 
 function getComputerChoice() { return choice[~~(Math.random() * choice.length)] }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase()
-  computerSelection = computerSelection.toLowerCase()
+function playRound(playerSelection) {
+  winnerElement.textContent = ""
+  computerSelection = getComputerChoice()
 
   if(playerSelection == computerSelection) { 
-    return "Draw! " +  capitalizeFirstLetter(playerSelection) + 
-           " and " + capitalizeFirstLetter(computerSelection)
+    setResults(`Draw! ${playerSelection} and ${computerSelection}`)
   }
   else if((playerSelection == "scissors" && computerSelection == "paper") || 
           (playerSelection == "paper" && computerSelection == "rock") ||
           (playerSelection == "rock" && computerSelection == "scissors"))
           { 
     playerScore++
-    return "You win! " + capitalizeFirstLetter(playerSelection) +
-           " beats " + capitalizeFirstLetter(computerSelection) 
+    setScore()
+    setResults(`You win! ${playerSelection} beats ${computerSelection}`)
   }
   else { 
     compScore++
-    return "You lose! " + capitalizeFirstLetter(computerSelection) + 
-           " beats " + capitalizeFirstLetter(playerSelection) 
+    setScore()
+    setResults(`You lose!  ${computerSelection} beats ${playerSelection}`)
+  }
+  if(playerScore === rounds) {
+    winnerElement.textContent = "You won. Humanity is saved."
+    toggleButtons()
+  }
+  else if (compScore === rounds) {
+    winnerElement.textContent = "AI won. Humanity is doomed."
+    toggleButtons()
   }
 }
 
-
-function game() {
-  while (true) {
-    let playerChoice=prompt("Rock, Paper or Scissors?")
-
-    console.log(playRound(playerChoice,getComputerChoice()))
-    console.log("Score:\nYou - " + playerScore + " Computer - " + compScore)
-
-    if(playerScore == drawTo) {
-      console.log("You won the game. Congrats..")
-      return
-    }
-    else if (compScore == drawTo) {
-      console.log("Computer won. Humanity is doomed.")
-      return
-    }
-  }
+function playAgain() {
+  playerScore = compScore = 0
+  setScore()
+  toggleButtons()
+  winnerElement.textContent=""
+  results.textContent="Do you have what it takes to stop AI from surpassing humanity?"
 }
 
-choice = ['Rock', 'Paper', 'Scissors']
-let playerScore = compScore = 0
-let drawTo = 5
-game()
+function toggleButtons() {
+  rock.toggleAttribute('disabled')
+  paper.toggleAttribute('disabled')
+  scissors.toggleAttribute('disabled')
+  playagainButton.toggleAttribute('hidden')
+}
+
+function setScore() {
+  playerScoreElement.textContent = playerScore
+  computerScoreElement.textContent = compScore
+}
+
+function setResults(text) { results.textContent = text }
